bc548ca73ceefed6b040be4962d5383673f25ef1
Vinicius Silva Augusto <100208158+ViniciusAugusto14@users.noreply.github.com>
2023-08-01 12:45:29 -0300

    Add files via upload

---
+++ FUNCOES/funcoes.js
@@ -0,0 +1,8 @@
+function dadosacesso(){
+    numcartao = prompt("Digite o numero do cartão")
+    senha = prompt("Digite a Senha")
+}
+function saque(valor){
+     saldo = saldo - valor
+     return saldo
+}
